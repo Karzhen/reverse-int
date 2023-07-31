1aba71134d4108224f806f37b38831ca819d96bc
Егор Черкашин <karzhen@MacBook-Pro-Egor.local>
2023-07-31 08:28:06 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let reversedNumber = Math.abs(number).toString().split('').reverse().join('');
+    return Number(reversedNumber);
 }
